f463381e11c4987a48a6916561adfad6c76d8f60
Jakesb27 <jake.boden27@gmail.com>
2023-02-25 22:11:57 -0800

    completed task 8.

--- EmployeeSQL/employee_queries.sql
+++ EmployeeSQL/employee_queries.sql
@@ -107,4 +107,12 @@ WHERE
 
 -- 8. List the frequency counts, in descending order, of all the employee last names
 --    (that is, how many employees share each last name).
+SELECT
+	emp.last_name "Last Name",
+	COUNT (emp.last_name) "Total"
+FROM
+	employees emp
+GROUP BY
+	emp.last_name
+ORDER BY "Total" DESC;
 
