03be0c9d17365b338a5e1a8e82174f011cfc39b8
Jakesb27 <jake.boden27@gmail.com>
2023-02-25 16:38:26 -0800

    updated schema files and names

--- EmployeeSQL/employees_db_schema.sql
+++ EmployeeSQL/employees_db_schema.sql
@@ -38,12 +38,18 @@ CREATE TABLE "salaries" (
 
 CREATE TABLE "dept_emp" (
     "emp_no" INT   NOT NULL,
-    "dept_no" VARCHAR(4)   NOT NULL
+    "dept_no" VARCHAR(4)   NOT NULL,
+    CONSTRAINT "pk_dept_emp" PRIMARY KEY (
+        "emp_no","dept_no"
+     )
 );
 
 CREATE TABLE "dept_manager" (
     "dept_no" VARCHAR(4)   NOT NULL,
-    "emp_no" INT   NOT NULL
+    "emp_no" INT   NOT NULL,
+    CONSTRAINT "pk_dept_manager" PRIMARY KEY (
+        "dept_no","emp_no"
+     )
 );
 
 ALTER TABLE "employees" ADD CONSTRAINT "fk_employees_emp_title_id" FOREIGN KEY("emp_title_id")
